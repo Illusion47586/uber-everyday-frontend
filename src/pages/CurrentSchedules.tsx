10782c8ef53c07dd6b647738d85d04663bbc8640
illusion <dhruvtiwari110058@gmail.com>
2022-03-27 15:38:16 +0530

    new page init

---
+++ src/pages/CurrentSchedules.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import s from "../styles/pages/base_page.module.scss";
+
+type Props = {};
+
+const CurrentSchedules = (props: Props) => {
+  return <div className={s.page}>CurrentSchedules</div>;
+};
+
+export default CurrentSchedules;
