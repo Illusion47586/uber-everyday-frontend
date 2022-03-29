111c7d1e7e5a6ce6a9ab59d9ae7a30d7df3b83aa
Vivaswat Sinha <vivaswatsinha@Vivaswats-MacBook-Pro.local>
2022-03-29 11:56:00 +0530

    added rides in currentSchedule

--- src/components/RideInfo.tsx
+++ src/components/RideInfo.tsx
@@ -1,5 +1,5 @@
 import { ArrowDown, ArrowRight, IconProps, IconWeight } from "phosphor-react";
-import { isValidElement, MouseEventHandler } from "react";
+import { isValidElement, MouseEventHandler, useState } from "react";
 import {
     Button,
     ButtonColor,
@@ -12,29 +12,73 @@ import styles from "../styles/components/rideinfo.module.scss";
 interface Props {
     source?: string;
     destination?: string;
+    showHr?: boolean;
+    data?: Array<string>;
 }
 
 const RideInfo = (props: Props) => {
+    const [expanded, setExpanded] = useState<any>(false);
 
     return (
-        <div className={styles.rides}>
-            <h5>Source: {props.source}</h5>
-            <h5>Destination: {props.destination}</h5>
-            <Button
-                text="Download Invoice"
-                icon={ArrowRight}
-                size={ButtonSize.small}
-                color={ButtonColor.Red}
-            ></Button>
-            <Button
-                text="View More"
-                icon={ArrowDown}
-                size={ButtonSize.small}
-                color={ButtonColor.Black}
-            ></Button>
+        <div className={styles.rideContainer}>
+            {props.showHr ? <hr /> : null}
 
+            <div className={styles.rides}>
+                <h5>Source: {props.source}</h5>
+                <h5>Destination: {props.destination}</h5>
+                <Button
+                    text="Download Invoice"
+                    icon={ArrowRight}
+                    size={ButtonSize.small}
+                    color={ButtonColor.Red}
+                ></Button>
+                <Button
+                    text="View More"
+                    icon={ArrowDown}
+                    size={ButtonSize.small}
+                    color={ButtonColor.Black}
+                    onClick={(event) => {
+                        if (expanded)
+                            setExpanded(false);
+                        else
+                            setExpanded(true);
+                    }}
+                ></Button>
+            </div>
 
-        </div>
+            {expanded ?
+                <div className={styles.expanded}>
+                    {/* Ride Id */}
+                    {/* source */}
+                    {/* destination */}
+                    {/* timing */}
+                    {/* total_traveler */}
+                    {/* start_date */}
+                    {/* end_date */}
+                    {/* cost */}
+                    {/* bill */}
+
+                    <h5 id={styles.rideid}>Ride Id: { }</h5>
+                    <h5 id={styles.source}>Source: { }</h5>
+                    <h5 id={styles.destination}>Destination: { }</h5>
+                    <h5 id={styles.timing}>Timing: { }</h5>
+                    <h5 id={styles.traveler}>Total Travelers: { }</h5>
+                    <h5 id={styles.start}>Start Date: { }</h5>
+                    <h5 id={styles.end}>End Date: { }</h5>
+                    {props.data ?
+                        <div className={styles.day_container}>
+                            <h5>Days of the Week Selected: </h5>
+                            <div className={styles.days}>
+                                {props.data.map((key) => <div>{key}</div>)}
+
+                            </div>
+                        </div>
+                        : null}
+                    <h5 id={styles.bill}>Bill: { }</h5>
+                </div>
+                : null}
+
+        </div >
     );
 };
 
--- src/pages/CurrentSchedules.tsx
+++ src/pages/CurrentSchedules.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import { TrendUp } from "phosphor-react";
+import React, { useState } from "react";
 import {
   Button,
   ButtonColor,
@@ -10,12 +11,14 @@ import { RideInfo } from "../components/RideInfo";
 import styles from "../styles/pages/current_schedule.module.scss";
 
 type Props = {};
+const test = ['1', '2', '3', '4', '5', '7'];
 
 const CurrentSchedules = (props: Props) => {
   return (
     <div className={styles.page}>
       <h1>Your Rides</h1>
-      <RideInfo />
+      <RideInfo showHr={false} data={test} />
+      <RideInfo showHr={true} />
     </div>
   );
 };
